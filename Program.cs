a657a7640c24c56a5b5a8493306a4e50d02da360
Joshua <Josh-T-Cox@outlook.com>
2026-03-13 18:45:33 +1000

    Initial game menu and setup flow

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class GameMenu
+{
+    public static int PromptForUser(string userRequest)
+    {
+        int menuOption;
+
+        while (true)
+        {
+            Console.WriteLine(userRequest);
+            Console.WriteLine();
+            Console.Write("> ");
+
+            if (int.TryParse(Console.ReadLine(), out menuOption))
+            {
+                if (menuOption == 1 || menuOption == 2)
+                {
+                    Console.WriteLine();
+                    return menuOption;
+                }
+            }
+
+            Console.WriteLine("Invalid input. Please try again.");
+            Console.WriteLine();
+        }
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        int mainChoice = GameMenu.PromptForUser("Press 1. Load Game\nPress 2. New Game");
+
+        if (mainChoice == 2)
+        {
+            int gameMode = GameMenu.PromptForUser("Press 1. Human vs Human\nPress 2. Human vs Computer");
+        }
+    }
+}
